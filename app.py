47ca45c6ae9140d6cc0ee42ce008b510e1a775cf
Jerry Hsu <Jerry>
2021-05-31 22:17:19 -0400

    Adoption Agency, Step 3: Create Add Pet Form

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, request, redirect, flash
 from flask_debugtoolbar import DebugToolbarExtension
+
 from models import db, connect_db, Pet
+from forms import AddPetForm
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "my-little-secret"
+app.config["SECRET_KEY"] = "secret_life_of_pets"
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///pet_adoption"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 debug = DebugToolbarExtension(app)
@@ -15,3 +17,22 @@ def homepage():
     """ homepage listing pets """
     pets = Pet.query.all()
     return render_template("homepage.html", pets=pets)
+
+@app.route("/add", methods=["GET", "POST"])
+def add_pet():
+    """ form for adding adoption candidate """
+    form = AddPetForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        species = form.species.data
+        photo_url = form.photo_url.data
+        age = form.age.data
+        notes = form.notes.data
+        pet = Pet(name=name,species=species,photo_url=photo_url,age=age,
+        notes=notes)
+
+        db.session.add(pet)
+        db.session.commit()
+        return redirect("/")
+    else:
+        return render_template("add_pet.html", form=form)
--- seed.py
+++ seed.py
@@ -4,9 +4,13 @@ from models import Pet
 db.drop_all()
 db.create_all()
 
-new_pet = Pet(name="Jackson", 
+jackson = Pet(name="Jackson", 
               species="American Eskimo",
               photo_url="https://bit.ly/2RbvKXI")
 
-db.session.add(new_pet)
+millie = Pet(name="Millie", 
+              species="Cavapoo",
+              photo_url="https://bit.ly/3vG9hAV")
+
+db.session.add_all([jackson, millie])
 db.session.commit()
